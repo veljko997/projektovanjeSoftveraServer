efe2a43f519d33f114e6d266046bb27728864bcd
Veljko Milosevic <veljkoroanda7@gmail.com>
2020-05-06 13:24:39 +0200

    Kreiran server.

--- src/services/server/ServiceReadDatabaseProperties.java
+++ src/services/server/ServiceReadDatabaseProperties.java
@@ -5,8 +5,10 @@
  */
 package services.server;
 
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 import org.apache.log4j.Logger;
@@ -18,14 +20,14 @@ import org.apache.log4j.Logger;
 public class ServiceReadDatabaseProperties {
 
     private final Properties properties;
-    private final String PROPERTIES_FILE_PATH="database.properties";
     private final String FILE_OUTPUT_STREAM="src/services/server/database.properties";
     private final Logger logger = Logger.getLogger(ServiceReadDatabaseProperties.class);
 
     public ServiceReadDatabaseProperties() {
         this.properties = new Properties();
         try {
-            properties.load(getClass().getResourceAsStream(PROPERTIES_FILE_PATH));
+            InputStream inputStream = new FileInputStream(FILE_OUTPUT_STREAM);
+            properties.load(inputStream);
         } catch (IOException ex) {
            logger.error(ex.getMessage());
         }
